9399a8557963b78b18c64ea82b503e5139fa572d
sirily11 <sirily1997@gmail.com>
2022-10-24 16:45:04 +0800

    fix: update color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,6 +24,14 @@ const theme = createTheme({
         },
       },
     },
+
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#fff",
+        },
+      },
+    },
   },
 });
 
